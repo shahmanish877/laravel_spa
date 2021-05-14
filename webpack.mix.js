ee712050ffc4406f675dc0dfb13b565cd540f226
Manish Shah <shahmanish877@gmail.com>
2021-05-14 17:25:38 +0545

    sds

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+require("laravel-mix-vue3");
+
+mix.vue3("resources/js/app.js", "public/js")
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
